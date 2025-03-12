3a70cd628685025df3e8c7f37665234ae14824a8
Chris Meadus <christopher.meadus@keyin.com>
2025-03-12 18:16:57 -0300

    Update and rename TestClass.java to Demo.java

--- problem-1/Demo.java
+++ problem-1/Demo.java
@@ -1,18 +1,18 @@
 public class TestClass {
     public static void main(String[] args) {
-        // Tests the person
+        // test person
         Person conor = new Person("Conor McGregor", 35, "M");
         System.out.println(conor);
 
-        // Test the student
+        // test student
         Student izzy = new Student("Israel Adesanya", 34, "M", "MMA001", 3.8);
         System.out.println(izzy);
 
-        // Test the teacher with a fixed salary of 65,999
+        // test teacher
         Teacher khabib = new Teacher("Khabib Nurmagomedov", 36, "M", "Wrestling", 65999);
         System.out.println(khabib);
 
-        // Test the college student
+        // test college student
         CollegeStudent jones = new CollegeStudent("Jon Jones", 36, "M", "MMA002", 3.9, 4, "Sports Science");
         System.out.println(jones);
     }
